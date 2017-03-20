a4b4456bbd02f68eedb53d8fc54087211a03bc82
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2017-03-20 17:34:40 +0700

    Add DB migration

---
+++ db/migrate/2017-03-14-111533-Create-Database.sql
@@ -0,0 +1,3 @@
+/* Setup database for test-fragmenta */
+CREATE USER "test-fragmenta_server" WITH PASSWORD 'bc0773dea68ecd21';
+CREATE DATABASE "test-fragmenta_development" WITH OWNER "test-fragmenta_server";
