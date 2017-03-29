61a9cd88a5efcb4e95ff341fef3fc170688d2674
joshua.salimin <13514001@std.stei.itb.ac.id>
2017-03-29 23:32:29 +0700

    integrasi html static kmrn ke revel

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -11,3 +11,39 @@ type App struct {
 func (c App) Index() revel.Result {
 	return c.Render()
 }
+
+func (c App) News() revel.Result {
+	return c.Render()
+}
+
+func (c App) Articles() revel.Result {
+	return c.Render()
+}
+
+func (c App) Files() revel.Result {
+	return c.Render()
+}
+
+func (c App) Photos() revel.Result {
+	return c.Render()
+}
+
+func (c App) Videos() revel.Result {
+	return c.Render()
+}
+
+func (c App) Profiles() revel.Result {
+	return c.Render()
+}
+
+func (c App) About() revel.Result {
+	return c.Render()
+}
+
+func (c App) Contact() revel.Result {
+	return c.Render()
+}
+
+func (c App) Login() revel.Result {
+	return c.Render()
+}
