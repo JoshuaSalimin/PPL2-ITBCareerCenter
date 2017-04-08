b16ecf61e72187851632bc7a8bdb6a03c3df3fc9
Azudeus <blitzthunder68@yahoo.com>
2017-04-08 22:44:32 +0700

    Added news wireframe

---
+++ app/controllers/news.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+)
+
+type News struct {
+	*revel.Controller
+}
+
+func (c News) Form() revel.Result {
+	return c.Render()
+}
+
+func (c News) Add() revel.Result {
+	return c.Render()
+}
