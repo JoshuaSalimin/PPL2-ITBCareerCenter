bc827857f9a1f7e6dd5dd9fd8baf56c16df91244
Azudeus <blitzthunder68@yahoo.com>
2017-04-10 06:01:45 +0700

    Add edit and delete template, Add delete functionality

--- app/controllers/news.go
+++ app/controllers/news.go
@@ -8,6 +8,7 @@ import (
     "github.com/go-gorp/gorp"
     "log"
     "time"
+    "strconv"
 )
 
 type News struct {
@@ -46,6 +47,42 @@ func (c News) Detail() revel.Result {
         return c.Render(news);
 }
 
+func (c News) Delete() revel.Result {
+        log.Println(c.Request.Form.Get("id"));
+        id,_ := strconv.Atoi(c.Request.Form.Get("id"));
+        log.Println(id);
+        success := DeleteNewsByNewsid(Dbm,id);
+        if (success){
+            return c.Redirect(News.List);
+        }else{
+            return c.Redirect(App.Index);        
+        }
+}
+
+func (c News) EditForm() revel.Result {
+        var id int;
+        c.Params.Bind(&id,"id");
+        news :=  SelectNewsByNewsId(Dbm,id);
+        return c.Render(news);
+}
+
+func (c News) EditSubmit() revel.Result {
+    innews := models.News{
+        NewsId : 0,
+        NewsTitle : c.Request.Form.Get("newstitle"),
+        Content : c.Request.Form.Get("newscontent"),
+        CreatedAt : time.Now().UnixNano(),
+        UpdatedAt : time.Now().UnixNano(), 
+    }
+    success := UpdateNews(Dbm, innews);
+    if (success){
+        return c.Redirect(News.List);
+    }else{
+        return c.Redirect(App.Index);        
+    }
+}
+
+
 func InsertNews(dbm *gorp.DbMap, p models.News) bool{
     err := dbm.Insert(&p)    
     checkErr(err, "Insert failed")    
@@ -62,9 +99,6 @@ func SelectAllNews(dbm *gorp.DbMap) []models.News {
     _, err := dbm.Select(&p, "SELECT * FROM News")
     checkErr(err, "Select failed")
     log.Println("All rows:")
-    for x, p := range p {
-        log.Printf("    %d: %v\n", x, p)
-    }
     return p 	
 }
 
@@ -72,18 +106,27 @@ func SelectNewsByNewsId(dbm *gorp.DbMap, newsid int) models.News {
 	var p models.News
     err := dbm.SelectOne(&p, "SELECT * FROM News WHERE newsid=?", newsid)
     checkErr(err, "SelectOne failed")
-    log.Println("p :", p)
     return p
 }
 
-func UpdateNews(dbm *gorp.DbMap, p models.News) {
+func UpdateNews(dbm *gorp.DbMap, p models.News) bool{
 	count, err := dbm.Update(&p)
 	checkErr(err, "Update failed")	
     log.Println("Rows updated:", count)
+    if(err == nil){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 
-func DeleteNewsByNewsid(dbm *gorp.DbMap, newsid int) {
+func DeleteNewsByNewsid(dbm *gorp.DbMap, newsid int) bool{
     _, err := dbm.Exec("DELETE FROM News WHERE newsid=?", newsid)
     checkErr(err, "Delete failed")
+    if(err == nil){
+        return true;
+    }else{
+        return false;
+    }
 }
