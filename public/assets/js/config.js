2ec7f9c1effa70d9a9edce6580e91bb8725c4e44
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2017-04-10 15:29:11 +0700

    Add file input template

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -3,15 +3,18 @@ requirejs.config({
     paths: {
         jquery: 'jquery.min',
         dropotron: 'jquery.dropotron.min',
+        bootstrap: 'bootstrap.min',
         pagination: 'jquery.simplePagination',
         skel: 'skel.min',
         util: 'util',
+        fileinput: 'fileinput.min',
         main: 'main'
     },
     shim: {
         'dropotron': ['jquery'],
         'pagination': ['jquery'],
         'util': ['jquery'],
+        'fileinput': ['jquery', 'bootstrap'],
         'main': ['jquery', 'skel', 'util']
     }
 });
