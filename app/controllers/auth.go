e25de8a8920f58ffaa396804af3ebe5dc1885281
alsoncahyadi <alson.cahyadi@gmail.com>
2017-04-12 04:02:05 +0700

    Login sanitized, select only respective uname and pwd, without proper front end

--- app/controllers/auth.go
+++ app/controllers/auth.go
@@ -13,24 +13,22 @@ type Auth struct {
 	*revel.Controller
 }
 
+type tai struct {}
+
 func (c Auth) Login() revel.Result {
+	loginFailedMsg := "Login Failed: "
+	log.Println(getCurrentUserId(c));
+
 	uname := c.Params.Form.Get("username")
 	pwd := c.Params.Form.Get("password")
-	log.Println("uname: " + uname + " | pwd: " + pwd)
-	users := SelectAllUsers(Dbm)
-	for _,user := range users {
-		if (user.Username == uname) {
-			if (user.Password == pwd) {
-				c.Session["cUserId"] = strconv.FormatInt(user.UserId, 10)
-				log.Println("MATCH !! => " + user.Username)
-				log.Println("session: " + c.Session["cUserId"])
-				return c.Redirect("/Login")
-			} else {
-				return c.Redirect("/Login")
-			}
-		} else {
-			log.Println("NO MATCH => " + user.Username)
-		}
+	user := SelectUserByUsernameAndPassword(Dbm, uname, pwd);
+	if (user.Username == uname) {
+		//Saves currently logged in user id in session, NIL OTHERWISE
+		c.Session["cUserId"] = strconv.FormatInt(user.UserId, 10)
+		c.Flash.Success("Login Successful");
+		return c.Redirect("/")
+	} else {
+		c.Flash.Error(loginFailedMsg + "Username atau Password salah");
 	}
 	return c.Redirect("/Login")
 }
@@ -41,19 +39,19 @@ func (c Auth) Logout() revel.Result {
 }
 
 /*
-func getCurrentUserId() int64 {
-	result, err := strconv.ParseInt(Controller.Session["cUserId"], 10, 64)
+func getCurrentUserId(c Auth) int64 {
+	result, err := strconv.ParseInt(c.Session["cUserId"], 10, 64)
 	if (err != nil) {
 		return -1;
 	}
 	return result;
 }
 
-func getCurrentUserRole() int {
-	cUserId := getCurrentUserId();
+func getCurrentUserRole(c Auth) int {
+	cUserId := getCurrentUserId(c);
 	if (cUserId == -1) {
 		return -1;
 	}
-	user := SelectUsersByUserid(Dbm, cUserId)
+	user := SelectUserByUserId(Dbm, cUserId)
 	return user.Role
 }*/
